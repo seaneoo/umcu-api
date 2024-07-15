aef1fc99fe78b6bd817aadb3c4c383bc2e89e296
Sean O'Connor <so@seano.dev>
2024-07-15 16:36:32 -0600

    Create error and exception handling

---
+++ src/main/kotlin/app/umcu/api/error/ErrorControllerImpl.kt
@@ -0,0 +1,40 @@
+/*
+ * UpcomingMCU API
+ * Copyright (C) 2024 The UpcomingMCU Project Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package app.umcu.api.error
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ErrorControllerImpl : ErrorController {
+
+	@RequestMapping("/error")
+	fun handleErrorMapping(req: HttpServletRequest, res: HttpServletResponse): ResponseEntity<ExceptionResponse> {
+		val httpStatus = HttpStatus.valueOf(res.status)
+		val response = ExceptionResponse(
+			statusCode = httpStatus.value(), statusReason = httpStatus.reasonPhrase, path = req.requestURI
+		)
+		return ResponseEntity.status(httpStatus).body(response)
+	}
+}
