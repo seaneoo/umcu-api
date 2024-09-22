7de9ad98884751d77a7040b573ba8bc52188032b
Sean O'Connor <so@seano.dev>
2024-09-22 10:08:11 -0600

    Bump Spring dependencies
    `org.springframework.boot` to 3.3.4
    `io.spring.dependency-management` to 1.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
 	kotlin("plugin.jpa") version "1.9.24"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
