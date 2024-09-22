d2bb73aecf090c9fc08fa8d94628426977cf5c74
Sean O'Connor <so@seano.dev>
2024-09-22 10:13:18 -0600

    Bump Kotlin to 1.9.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.24"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "app.umcu"
